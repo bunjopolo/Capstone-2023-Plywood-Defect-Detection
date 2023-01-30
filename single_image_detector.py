1e6d7fbdcdf604759fbf6c3242d6f12d2c7da244
bunjopolo <76890407+bunjopolo@users.noreply.github.com>
2023-01-29 19:59:55 -0800

    Update single_image_detector.py

--- single_image_detector.py
+++ single_image_detector.py
@@ -1,63 +1,60 @@
-import os
-import cv2
-import numpy as np
-from detectron2 import model_zoo
-from detectron2.config import get_cfg
-from detectron2.data import MetadataCatalog
-from detectron2.data.catalog import DatasetCatalog
-from detectron2.data.datasets import register_coco_instances
-from detectron2.engine import DefaultPredictor
-from detectron2.utils.visualizer import Visualizer
-
-
 class Detectron_Detector(): 
     def __init__(self):
-        #initialize variables
+        # initialize variables for the class
         self.config ='COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml'
         self.num_classes = 4
         self.device ='cpu'
         self.im_num = 1
 
-    #create predictor object 
+    # create a predictor object 
     def make_predictor(self, weights, score_thresh):
+        # create a configuration object and load the specified configuration file
         cfg = get_cfg()
         cfg.merge_from_file(model_zoo.get_config_file(self.config))
+
+        # set the weights of the model, the number of classes, score threshold for predictions, and the device to run on
         cfg.MODEL.WEIGHTS = os.path.join(weights)
         cfg.MODEL.ROI_HEADS.NUM_CLASSES = self.num_classes
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = score_thresh
         cfg.MODEL.DEVICE = self.device
+        
+        # initialize the predictor with the configuration object
         self.predictor = DefaultPredictor(cfg)
 
-    #checks input image for correct dimensions 
+    # check the input image for correct dimensions 
     def check_image_dimensions(self, image):
         height, width = image.shape[:2]  
-        #return same image if correct 
+        # return the same image if it has the correct dimensions
         if width == 2048 and height == 1536:
             return image
-        #resize if incorrect
+        # resize the image if it has incorrect dimensions
         else:
             print('image size incorrect, resizing it for you...')
             image = cv2.resize(image, (2048, 1536)) 
-            return image)
-      
-    #run prediction and return numpy array of bounding boxes 
+            return image
+
+    # run prediction and return a numpy array of bounding boxes 
     def predict(self, image_path): 
         self.image_path = image_path
         self.img = cv2.imread(image_path)
         self.img = self.check_image_dimensions(self.img)
         self.outputs = self.predictor(self.img)
         out = self.outputs
+        # extract the predicted bounding boxes from the outputs
         pred_boxes = out["instances"].pred_boxes.tensor.numpy()
         return pred_boxes
-    
+
+    # find the center of each bounding box
     def find_centers(self, box):
         centers = np.zeros((len(box), 2))
         for i in range(len(box)): 
+            # calculate the x and y coordinates of the center of the bounding box
             x = (box[i][0] + box[i][2])/2
             y = (box[i][1] + box[i][3])/2
             centers[i] = [x, y]
         return centers
 
+    #display image with bounding boxes 
     def show(self): 
         image = self.img
         vis = Visualizer(image[:, :, ::-1], MetadataCatalog.get(self.config), scale=2)
